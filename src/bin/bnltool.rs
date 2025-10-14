e9245525ed2937fff5a209228856f136c7beed3a
luboise <lucas2@live.com.au>
2025-10-15 05:07:25 +1100

    Updated CLI to use clap

--- src/bin/bnltool.rs
+++ src/bin/bnltool.rs
@@ -1,108 +1,142 @@
 use std::{
-    env,
     ffi::OsStr,
     fs,
     path::{Path, PathBuf},
 };
 
 use bnl::BNLFile;
+use clap::{Parser, Subcommand, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    propagate_version = true,
+    subcommand_required = true,
+    arg_required_else_help = true
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(name = "extract", short_flag = 'x')]
+    /// Extract an existing BNL file
+    Extract {
+        /// The .bnl file to extract
+        #[arg(value_name = "BNL")]
+        bnl_file: PathBuf,
+
+        /// The output directory for the extracted files
+        #[arg(short = 'd', default_value = "./out")]
+        output_dir: PathBuf,
+    },
+
+    #[command(short_flag = 'c')]
+    /// Create a new BNL file from one or more directories which contain loose assets.
+    Create {
+        /// The directories containing the assets
+        asset_dirs: Vec<PathBuf>,
+
+        #[arg(short = 'o', value_name = "FILE")]
+        /// The path which the new .bnl file will be written to
+        output_file: PathBuf,
+    },
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    // TODO: Refactor this to use a CLI args crate if this gets worked on more
-    if args.len() != 3 {
-        print_usage();
-        return;
-    }
+    match cli.command {
+        Commands::Extract {
+            bnl_file,
+            output_dir,
+        } => {
+            println!("Opening BNL file {}", bnl_file.display());
 
-    if &args[1].to_lowercase() != "-x" {
-        eprintln!("Expected -x as second argument.");
-        error_exit(true);
-    }
-
-    let bnl_path = PathBuf::from(&args[2]);
-
-    println!("Opening BNL file {}", bnl_path.display());
-
-    let bytes: Vec<u8> = match std::fs::read(&bnl_path) {
-        Ok(f) => f,
-        Err(e) => {
-            println!("Unable to open file {}. Error: {}", bnl_path.display(), e);
-            return;
-        }
-    };
-
-    let bnl = match BNLFile::from_bytes(&bytes) {
-        Ok(b) => b,
-        Err(e) => {
-            eprintln!("Unable to process BNL file: {:?}", e);
-
-            error_exit(false);
-        }
-    };
-
-    let raw_assets = bnl.get_raw_assets();
-
-    let out_filename = format!(
-        "{}_bnl",
-        bnl_path
-            .file_stem()
-            .unwrap_or(OsStr::new("unknown"))
-            .display()
-    );
-
-    // ./out/common_bnl
-    let bnl_out_path = Path::new("./out").join(out_filename);
-
-    raw_assets.iter().for_each(|raw_asset| {
-        // ./out/common_bnl/aid_texture_xyz
-        let asset_path: PathBuf = bnl_out_path.join(raw_asset.name());
-
-        if asset_path.is_file() {
-            eprintln!(
-                "Unable to write to {} (A file already exists by that name)",
-                asset_path.display()
-            );
-            return;
-        } else if !asset_path.exists() {
-            match fs::create_dir_all(&asset_path) {
-                Ok(_) => (),
+            let bytes: Vec<u8> = match std::fs::read(&bnl_file) {
+                Ok(f) => f,
                 Err(e) => {
-                    eprintln!(
-                        "Unable to create directory {}.\nError: {}",
-                        asset_path.display(),
-                        e
-                    );
+                    println!("Unable to open file {}. Error: {}", bnl_file.display(), e);
                     return;
                 }
-            }
-        }
+            };
 
-        std::fs::write(asset_path.join("descriptor"), raw_asset.descriptor_bytes()).unwrap_or_else(
-            |e| {
-                eprintln!(
-                    "Unable to write descriptor for {}\nError: {}",
-                    &raw_asset.name(),
-                    e
-                );
-            },
-        );
+            let bnl = match BNLFile::from_bytes(&bytes) {
+                Ok(b) => b,
+                Err(e) => {
+                    eprintln!("Unable to process BNL file: {:?}", e);
 
-        if let Some(data_slices) = raw_asset.resource_chunks() {
-            data_slices.iter().enumerate().for_each(|(i, slice)| {
-                std::fs::write(asset_path.join(format!("resource{}", i)), slice).unwrap_or_else(
-                    |e| {
+                    error_exit(false);
+                }
+            };
+
+            let raw_assets = bnl.get_raw_assets();
+
+            let out_filename = format!(
+                "{}_bnl",
+                bnl_file
+                    .file_stem()
+                    .unwrap_or(OsStr::new("unknown"))
+                    .display()
+            );
+
+            // ./out/common_bnl
+            let bnl_out_path = Path::new(&output_dir).join(out_filename);
+
+            raw_assets.iter().for_each(|raw_asset| {
+                // ./out/common_bnl/aid_texture_xyz
+                let asset_path: PathBuf = bnl_out_path.join(raw_asset.name());
+
+                if asset_path.is_file() {
+                    eprintln!(
+                        "Unable to write to {} (A file already exists by that name)",
+                        asset_path.display()
+                    );
+                    return;
+                } else if !asset_path.exists() {
+                    match fs::create_dir_all(&asset_path) {
+                        Ok(_) => (),
+                        Err(e) => {
+                            eprintln!(
+                                "Unable to create directory {}.\nError: {}",
+                                asset_path.display(),
+                                e
+                            );
+                            return;
+                        }
+                    }
+                }
+
+                std::fs::write(asset_path.join("descriptor"), raw_asset.descriptor_bytes())
+                    .unwrap_or_else(|e| {
                         eprintln!(
                             "Unable to write descriptor for {}\nError: {}",
-                            raw_asset.name(),
+                            &raw_asset.name(),
                             e
                         );
-                    },
-                );
+                    });
+
+                if let Some(data_slices) = raw_asset.resource_chunks() {
+                    data_slices.iter().enumerate().for_each(|(i, slice)| {
+                        std::fs::write(asset_path.join(format!("resource{}", i)), slice)
+                            .unwrap_or_else(|e| {
+                                eprintln!(
+                                    "Unable to write descriptor for {}\nError: {}",
+                                    raw_asset.name(),
+                                    e
+                                );
+                            });
+                    });
+                }
             });
         }
-    });
+        Commands::Create {
+            asset_dirs,
+            output_file,
+        } => println!("Not implemented yet!"),
+    }
 }
 
 fn print_usage() {
