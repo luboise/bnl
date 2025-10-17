77a79fc56a793de0395ad09501f7c52e6fe506f3
luboise <lucas2@live.com.au>
2025-10-17 15:22:39 +1100

    Added miniwavebankformat 3

--- src/xsb.rs
+++ src/xsb.rs
@@ -60,10 +60,9 @@ pub fn wav_files_from_path(path: PathBuf) -> Result<Vec<WavFile>, Box<dyn Error>
         for i in 0..num_wav_entries as usize {
             let raw_entry = RawWavEntry {
                 unknown_1: cur.read_u32::<LittleEndian>()?,
-                flag1: cur.read_u8()?,
-                flag2: cur.read_u8()?,
-                flag3: cur.read_u8()?,
-                flag4: cur.read_u8()?,
+
+                raw_format: cur.read_u32::<LittleEndian>()?,
+
                 bytes_ptr: cur.read_u32::<LittleEndian>()?,
                 num_bytes: cur.read_u32::<LittleEndian>()?,
                 unknown_2: cur.read_u32::<LittleEndian>()?,
@@ -131,14 +130,101 @@ pub(crate) struct Wavebank {
 
 const RAW_WAV_ENTRY_SIZE: usize = 5 * size_of::<u32>() + 4 * size_of::<u8>();
 
+/// Microsoft WAVEBANKMINIWAVEFORMAT
+/// https://learn.microsoft.com/en-us/previous-versions/bb206350(v=vs.85)
+#[derive(Debug, Clone)]
+pub struct WaveBankMiniWaveFormat1 {
+    /// false for uncompressed PCM, true for compressed
+    pub is_compressed: bool,
+    /// Number of audio channels
+    pub num_channels: u8,
+    /// 27 bit value representing number of samples per second
+    pub samples_per_sec: u32,
+    /// Indicates 16 byte samples when true
+    pub uses_wide_format: bool,
+}
+
+impl WaveBankMiniWaveFormat1 {
+    fn new(dword: u32) -> Self {
+        // DWORD wFormatTag : 1;
+        let is_compressed: bool = dword & 0x1 == 1;
+
+        // DWORD nChannels : 3;
+        let num_channels: u8 = (dword >> 1) as u8 & 0b111;
+
+        // DWORD nSamplesPerSec : 27;
+        let samples_per_sec: u32 = (dword >> (1 + 3)) & 0x1FFFFFF; // 27 bits
+
+        // DWORD wBitsPerSample : 1;
+        let uses_wide_format: bool = ((dword >> (1 + 3 + 27)) & 1u32) == 1;
+
+        Self {
+            is_compressed,
+            num_channels,
+            samples_per_sec,
+            uses_wide_format,
+        }
+    }
+}
+
+/// Wine WAVEBANKMINIWAVEFORMAT
+/// https://source.winehq.org/source/include/xact3wb.h
+#[derive(Debug, Clone)]
+pub struct WaveBankMiniWaveFormat3 {
+    /// DWORD wFormatTag : 2
+    format_tag: u8,
+    /// DWORD nChannels : 3;
+    num_channels: u8,
+    /// DWORD nSamplesPerSec : 18;
+    samples_per_sec: u32,
+    /// DWORD wBlockAlign    :  8;
+    block_align: u8,
+    /// DWORD wBitsPerSample :  1;
+    uses_wide_format: bool,
+}
+
+impl Default for WaveBankMiniWaveFormat3 {
+    fn default() -> Self {
+        Self {
+            format_tag: 0,
+            num_channels: 2,
+            samples_per_sec: 44100,
+            block_align: 200,
+            uses_wide_format: true,
+        }
+    }
+}
+
+impl WaveBankMiniWaveFormat3 {
+    fn new(dword: u32) -> Self {
+        let format_tag: u8 = (dword & 0b11) as u8;
+
+        // DWORD nChannels : 3
+        let num_channels: u8 = (dword >> 2) as u8 & 0b111;
+
+        // DWORD nSamplesPerSec : 18;
+        let samples_per_sec: u32 = (dword >> (2 + 3)) & 0x3FFFF; // 18 bits
+
+        let block_align: u8 = (dword >> (2 + 3 + 18)) as u8; // 8 bits
+
+        // DWORD wBitsPerSample : 1;
+        let uses_wide_format: bool = ((dword >> (2 + 3 + 18 + 8)) & 1u32) == 1;
+
+        Self {
+            format_tag,
+            num_channels,
+            samples_per_sec,
+            block_align,
+            uses_wide_format,
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Default, Clone)]
 pub(crate) struct RawWavEntry {
     unknown_1: u32,
 
-    flag1: u8,
-    flag2: u8,
-    flag3: u8,
-    flag4: u8,
+    raw_format: u32,
 
     bytes_ptr: u32,
     num_bytes: u32,
@@ -150,10 +236,7 @@ pub(crate) struct RawWavEntry {
 pub struct WavFile {
     unknown_1: u32,
 
-    flag1: u8,
-    flag2: u8,
-    flag3: u8,
-    flag4: u8,
+    format: WaveBankMiniWaveFormat3,
 
     bytes: Vec<u8>,
 
@@ -165,10 +248,8 @@ impl WavFile {
     pub(crate) fn from_raw(raw: RawWavEntry, bytes: Vec<u8>) -> Self {
         Self {
             unknown_1: raw.unknown_1,
-            flag1: raw.flag1,
-            flag2: raw.flag2,
-            flag3: raw.flag3,
-            flag4: raw.flag4,
+
+            format: WaveBankMiniWaveFormat3::new(raw.raw_format),
             bytes,
             unknown_2: raw.unknown_2,
             unknown_3: raw.unknown_3,
@@ -196,8 +277,13 @@ impl WavFile {
             .collect::<Vec<f32>>();
         */
 
-        wavers::write(out_path, &samples, 44100, 1)
-            .map_err(|_| io::Error::other("Failed to write wav file."))
+        wavers::write(
+            out_path,
+            &samples,
+            (self.format.samples_per_sec / self.format.num_channels as u32) as i32,
+            self.format.num_channels.into(),
+        )
+        .map_err(|_| io::Error::other("Failed to write wav file."))
     }
 
     pub fn dump_raw<P: AsRef<Path>>(&self, out_path: P) -> Result<(), io::Error> {
@@ -206,5 +292,51 @@ impl WavFile {
         fs::write(out_path, &self.bytes)?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn wavebank_mini_format_de_mono() {
+        let dword = u32::from_le_bytes([0x44, 0xc4, 0x0a, 0x80]);
+
+        let format = WaveBankMiniWaveFormat3::new(dword);
+        dbg!(&format);
+
+        assert_eq!(format.num_channels, 1, "Should be mono.");
+        assert_eq!(format.format_tag, 0, "Format should be PCM.");
+        assert_eq!(
+            format.samples_per_sec, 22050,
+            "Sample rate should be 22050."
+        );
+
+        /*
+        assert_eq!(
+            format.samples_per_sec, 44100,
+            "Sample rate should be 44100."
+        );
+
+        */
+        assert!(format.uses_wide_format, "Wide format should be true.")
+    }
+
+    #[test]
+    fn wavebank_mini_format_de_stereo() {
+        let dword = u32::from_le_bytes([0x88, 0x88, 0x15, 0x80]);
+
+        let format = WaveBankMiniWaveFormat3::new(dword);
+        dbg!(&format);
+
+        assert_eq!(format.num_channels, 2, "Should be stereo.");
+        assert_eq!(format.format_tag, 0, "Shouldn't be compressed.");
+        assert_eq!(
+            format.samples_per_sec, 44100,
+            "Sample rate should be 44100."
+        );
+
+        assert!(format.uses_wide_format, "Wide format should be true.")
     }
 }
